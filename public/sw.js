3015fe258eff0855200a033554fe2100627d63ea
lilgafferz-ai <lilgafferz@email.com>
2026-06-05 11:46:44 +0300

    add PWA support

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+﻿const CACHE = 'blitzmall-v1';
+const ASSETS = ['/', '/index.html', '/static/js/main.chunk.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
